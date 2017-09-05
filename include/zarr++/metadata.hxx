68decb7ec42553eefa90ee51652a083e2b3ca7f5
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2017-09-04 21:06:20 -0400

    Update metadata

--- include/zarr++/metadata.hxx
+++ include/zarr++/metadata.hxx
@@ -9,14 +9,35 @@
 
 namespace zarr {
 
-    struct Metadata {
+    class Metadata {
+
+    public:
+        Metadata(const int zarr_format) : zarr_format_(zarr_format) {
+
+        }
+
+    private:
+        int zarr_format_;
+    };
+
+    class ArrayMetadata : public Metadata {
+
+    public:
 
     private:
         types::ShapeType shape_;
-    };
+        types::ShapeType chunks_;
+        // FIXME this should be defined in types
+        std::string dtype_;
+        nlohman::json::json compressor_;
+        // TODO
+        FillValue fill_value_;
+        std::string order_;
+        nlohman::json::json filter_;
+    }
 
     void writeMetaData(const handle::Handle & handle, const Metadata & metadata) {
-        
+
     }
 
     void readMetaData(const handle::Handle & handle, Metadata & metadata) {
