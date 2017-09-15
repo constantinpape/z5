d1ffcb36e53b0e9229603bcc181f0874b8288700
Constantin Pape <c.pape@gmx.net>
2017-09-15 00:40:33 -0400

    Start conda recipe

---
+++ conda-recipe/build.sh
@@ -0,0 +1,7 @@
+#
+# set the correct python flags, depending on the distribution
+#
+
+PY_VER=$(python -c "import sys; print('{}.{}'.format(*sys.version_info[:2]))")
+PY_ABIFLAGS=$(python -c "import sys; print('' if sys.version_info.major == 2 else sys.abiflags)")
+PY_ABI=${PY_VER}${PY_ABIFLAGS}
