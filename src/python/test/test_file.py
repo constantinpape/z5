5a23e7867ba19499e06f22f5e2204f335d468390
Chris Barnes <barnesc@janelia.hhmi.org>
2018-03-06 16:09:31 -0500

    unit tests for format inference

--- src/python/test/test_file.py
+++ src/python/test/test_file.py
@@ -13,13 +13,14 @@ except ImportError:
 class TestFile(unittest.TestCase):
     def setUp(self):
         self.n5_path = 'array.n5'
-        self.zarr_path = 'array.zr'
+        self.zr_path = 'array.zr'
+        self.zarr_path = 'array.zarr'
 
     def tearDown(self):
         if(os.path.exists(self.n5_path)):
             rmtree(self.n5_path)
-        if(os.path.exists(self.zarr_path)):
-            rmtree(self.zarr_path)
+        if(os.path.exists(self.zr_path)):
+            rmtree(self.zr_path)
 
     def test_context_manager_n5(self):
         self.assertFalse(os.path.exists(self.n5_path))
@@ -31,10 +32,28 @@ class TestFile(unittest.TestCase):
         self.assertTrue(os.path.exists(self.n5_path))
 
     def test_context_manager_zarr(self):
-        self.assertFalse(os.path.exists(self.zarr_path))
+        self.assertFalse(os.path.exists(self.zr_path))
 
-        with z5py.File(self.zarr_path, True) as f:
+        with z5py.File(self.zr_path, True) as f:
             self.assertIsInstance(f, z5py.File)
             self.assertTrue(f.is_zarr)
 
-        self.assertTrue(os.path.exists(self.zarr_path))
+        self.assertTrue(os.path.exists(self.zr_path))
+
+    def test_extension_detect_n5(self):
+        self.assertFalse(os.path.exists(self.n5_path))
+
+        f = z5py.File(self.n5_path, None)
+        self.assertFalse(f.is_zarr)
+
+    def test_extension_detect_zr(self):
+        self.assertFalse(os.path.exists(self.zr_path))
+
+        f = z5py.File(self.zr_path, None)
+        self.assertTrue(f.is_zarr)
+
+    def test_extension_detect_zarr(self):
+        self.assertFalse(os.path.exists(self.zarr_path))
+
+        f = z5py.File(self.zarr_path, None)
+        self.assertTrue(f.is_zarr)
