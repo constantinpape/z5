87433ae30c466f309789b1502e795188c0df119a
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2018-06-17 15:29:55 +0200

    Enable zarr format in 'rechunk' and add documentation

--- src/python/module/z5py/util.py
+++ src/python/module/z5py/util.py
@@ -8,6 +8,8 @@ from .file import File
 
 # ND blocking generator
 def blocking(shape, block_shape):
+    """ Generator for nd blocking
+    """
     if len(shape) != len(block_shape):
         raise RuntimeError("Invalid number of dimensions.")
     ranges = [range(sha // bsha if sha % bsha == 0 else sha // bsha + 1)
@@ -19,9 +21,6 @@ def blocking(shape, block_shape):
                     for pos, bsha, sha in zip(positions, block_shape, shape))
 
 
-# TODO zarr support
-# rechunk a n5 dataset
-# also supports new compression opts
 def rechunk(in_path,
             out_path,
             in_path_in_file,
@@ -30,9 +29,36 @@ def rechunk(in_path,
             n_threads,
             out_blocks=None,
             dtype=None,
+            use_zarr_format=None,
             **new_compression):
-    f_in = File(in_path, use_zarr_format=False)
-    f_out = File(out_path, use_zarr_format=False)
+    """ Copy and rechunk a dataset
+
+    Copy a given dataset and rechunk it.
+    Allows to change datatype, file format and compression as well.
+
+    Args:
+        in_path (str): path to the input file
+        out_path (str): path to the output file
+        in_path_in_file (str): name of input dataset
+        out_path_in_file (str): name of output dataset
+        out_chunks (tuple): chunks of the output dataset
+        n_threads (int): number of threads used for copying
+        out_blocks (tuple): blocks used for copying. Must be a multiple
+                            of ``out_chunks``, which are used by default (default: None)
+        dtype (str): datatype of the output dataset, default does not change datatype (default: None)
+        use_zarr_format (bool): file format of the output file,
+                                default does not change format (default: None)
+    Kwargs:
+        new_compression: compression library and options for output dataset. If not given,
+                         the same compression as in the input is used.
+
+    """
+    f_in = File(in_path)
+    # check if the file format was specified
+    # if not, keep the format of the input file
+    # otherwise set the file format
+    is_zarr = f_in.is_zarr if use_zarr_format is None else use_zarr_format
+    f_out = File(out_path, use_zarr_format=is_zarr)
 
     # if we don't have out-blocks explitictly given,
     # we iterate over the out chunks
