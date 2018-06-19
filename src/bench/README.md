ece162cea2b20d558604c8b48ce23db5ba1efa03
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2018-06-19 19:07:21 +0200

    Implement java benchmarks

--- src/bench/README.md
+++ src/bench/README.md
@@ -1,1 +1,4 @@
 # Benchmarks
+
+- bench_python: Python benchmarks comparing performance with h5py.
+- bench_java: Re-implementation of n5-java benchmarks: https://github.com/saalfeldlab/n5/blob/master/src/test/java/org/janelia/saalfeldlab/n5/N5Benchmark.java
---
+++ src/bench/bench_java/n5_benchmark.py
@@ -0,0 +1,108 @@
+import os
+import zipfile
+import time
+
+from concurrent import futures
+from subprocess import call
+from shutil import rmtree
+# from imageio import imread
+from skimage import io
+
+import numpy as np
+import z5py
+from z5py.dataset import Dataset
+
+BENCH_DIR = 'bench_dir'
+IM_URL = "https://imagej.nih.gov/ij/images/t1-head-raw.zip"
+
+
+def set_up():
+    """
+    Make the tmp directory and load the test image
+    """
+    if os.path.exists(BENCH_DIR):
+        rmtree(BENCH_DIR)
+    os.mkdir(BENCH_DIR)
+
+    im_file = os.path.join(BENCH_DIR, 'im.zip')
+    call(['wget', '-O', im_file, IM_URL])
+
+    with zipfile.ZipFile(im_file) as f:
+        f.extract('JeffT1_le.tif', BENCH_DIR)
+
+    im_file = os.path.join(BENCH_DIR, 'JeffT1_le.tif')
+    im = np.array(io.imread(im_file))
+    return im[30:94, 100:164, 100:164].astype('uint8')
+
+
+def benchmark_writing_speed(data):
+    n_blocks = 5
+    n_reps = 1
+
+    shape = 3 * (n_blocks * 64,)
+    chunks = 3 * (64,)
+
+    compression_methods = Dataset.compressors_n5
+    f = z5py.File(os.path.join(BENCH_DIR, 'f1.n5'))
+
+    for n in range(n_reps):
+        for compression in compression_methods:
+            t0 = time.time()
+            ds = f.create_dataset('ds_%s' % compression, shape=shape,
+                                  chunks=chunks, compression='gzip', dtype='uint8')
+            for i in range(n_blocks):
+                for j in range(n_blocks):
+                    for k in range(n_blocks):
+                        ds[i*64:(i + 1)*64,
+                           j*64:(j + 1)*64,
+                           k*64:(k + 1)*64] = data
+            t1 = time.time()
+            t_diff = t1 - t0
+            print("%i : %s : %f s" % (n, compression, t_diff))
+
+
+def benchmark_parallel_writing_speed(data):
+    n_blocks = 5
+
+    shape = 3 * (n_blocks * 64,)
+    chunks = 3 * (64,)
+
+    compression_methods = Dataset.compressors_n5
+    f = z5py.File(os.path.join(BENCH_DIR, 'f2.n5'))
+
+    for n_threads in range(1, 9):
+        n_threads *= 2
+        print(n_threads, 'threads.')
+        for compression in compression_methods:
+
+            t0 = time.time()
+            ds = f.create_dataset('ds_%s_%i' % (compression, n_threads), shape=shape,
+                                  chunks=chunks, compression='gzip', dtype='uint8')
+
+            def write_chunk(i, j, k):
+                ds[i*64:(i + 1)*64,
+                   j*64:(j + 1)*64,
+                   k*64:(k + 1)*64] = data
+
+            with futures.ThreadPoolExecutor(n_threads) as tp:
+                tasks = [tp.submit(write_chunk, i, j, k) for i in range(n_blocks)
+                         for j in range(n_blocks) for k in range(n_blocks)]
+                [t.result() for t in tasks]
+
+            t1 = time.time()
+            t_diff = t1 - t0
+            print("%i : %s : %f s" % (n_threads, compression, t_diff))
+
+
+def clean_up():
+    try:
+        rmtree(BENCH_DIR)
+    except OSError:
+        pass
+
+
+if __name__ == '__main__':
+    data = set_up()
+    benchmark_writing_speed(data)
+    benchmark_parallel_writing_speed(data)
+    clean_up()
