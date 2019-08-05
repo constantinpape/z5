926fa24e74d65c306466918e3d935505d29029ae
Constantin Pape <c.pape@gmx.net>
2019-08-05 16:38:19 +0200

    Bump version

--- src/python/module/z5py/__init__.py
+++ src/python/module/z5py/__init__.py
@@ -1,6 +1,6 @@
 from .file import File, N5File, ZarrFile
 from .attribute_manager import set_json_encoder, set_json_decoder
 
-__version__ = "1.4.1"
+__version__ = "1.5.0"
 __version_info__ = tuple(int(i) for i in  __version__.split("."))
 
