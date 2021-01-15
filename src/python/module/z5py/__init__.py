4fc6dddc9a4a7e60bb07259f10c408bc2f9d563b
Constantin Pape <c.pape@gmx.net>
2021-01-15 13:33:03 +0100

    Bump version

--- src/python/module/z5py/__init__.py
+++ src/python/module/z5py/__init__.py
@@ -3,5 +3,5 @@ from .dataset import Dataset
 from .group import Group
 from .attribute_manager import set_json_encoder, set_json_decoder
 
-__version__ = "2.0.6"
+__version__ = "2.0.7"
 __version_info__ = tuple(int(i) for i in __version__.split("."))
